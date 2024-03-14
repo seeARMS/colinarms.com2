62309ccd0032e645b769519ed721084564cbfc5c
Colin Armstrong <colinarms@gmail.com>
2024-03-13 20:52:12 -0700

    better mobile

--- src/pages/photography.jsx
+++ src/pages/photography.jsx
@@ -140,9 +140,11 @@ function Modal({ src, open, setOpen}) {
     unmount={false}
 
 
+
     >
       <Dialog as="div" className="relative z-10" onClose={setOpen}
     unmount={false}
+
 
     >
         <Transition.Child
@@ -157,11 +159,21 @@ function Modal({ src, open, setOpen}) {
 
         >
 
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div
+    className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+
+
+    />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto"
+
+
+    >
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
+
+    onClick={ () => setOpen(false)}
+    >
             <Transition.Child
     unmount={false}
               as={Fragment}
@@ -172,7 +184,7 @@ function Modal({ src, open, setOpen}) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white  text-left shadow-xl transition-all  sm:w-full sm:m-32 "
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white  text-left shadow-xl transition-all  sm:w-full md:m-32 "
 
     >
                 <div>
@@ -184,6 +196,9 @@ function Modal({ src, open, setOpen}) {
                       quality={100}
                       placeholder="blur"
                       priority
+
+    onClick={ () => setOpen(false)}
+
                       />
                     </div>
                   </div>
