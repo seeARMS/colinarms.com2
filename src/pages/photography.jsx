adaa5c1fc2c283ce444c0955f50a58f5fe677e76
Colin Armstrong <colinarms@gmail.com>
2024-03-13 18:15:38 -0700

    Faster image loading

--- src/pages/photography.jsx
+++ src/pages/photography.jsx
@@ -214,7 +214,9 @@ export default function Photography() {
           <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-teal-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 transition-all"
 
         >
-            <Image src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75 transition-all" fill
+            <Image
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+         src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75 transition-all" fill
         />
         {/*<button type="button" className="absolute inset-0 focus:outline-none">
               <span className="sr-only">View details for {file.title}</span>
